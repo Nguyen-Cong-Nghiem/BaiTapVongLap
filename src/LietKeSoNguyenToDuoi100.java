fedbbd96b25e24b6bba0612db20061401bea8d21
Nguyen-Cong-Nghiem <kukull.ncn@gmail.com>
2018-06-18 12:55:21 +0700

    Add file Liet ke nguyen to

---
+++ src/LietKeSoNguyenToDuoi100.java
@@ -0,0 +1,17 @@
+public class LietKeSoNguyenToDuoi100 {
+    public static void main(String[] args) {
+        for(int a=2;a<100;a++){
+            int i=2;
+            boolean check =true;
+            while(i<= Math.sqrt(a)){
+                if(a%i==0)
+                    check = false;
+                break;
+            }
+            i++;
+            if(check)
+                System.out.println(a);
+        }
+
+    }
+}
